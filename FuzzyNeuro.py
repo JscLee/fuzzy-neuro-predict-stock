ef3ef97b4efb0c8caff353b0bdbd4e917bb34078
jsc lee <jsclee2013@163.com>
2017-04-14 15:58:52 +0800

    运行中可调迭代次数和学习速率

--- FuzzyNeuro.py
+++ FuzzyNeuro.py
@@ -13,10 +13,16 @@ import random
 from os.path import exists
 from os import mkdir
 
-dir='C:\\Projects\\FuzzyNeuro\\FuzzyNeuro\\20170330\\5'
+dir='C:\\Projects\\FuzzyNeuro\\FuzzyNeuro\\20170414\\1'
+pred=[]
+outputs=[]
+
 if not exists(dir):
     mkdir(dir)
-
+def plot_pre():
+    plot(pred)
+    plot(outputs)
+    show()
 def shuffle_exp(inputs,outputs,i):
     random.seed(i)
     random.shuffle(inputs,random.random)
@@ -29,31 +35,30 @@ def layout_1(inputs,outputs):
     #x.tag.test_value=inputs[:3]
     #x.tag.test_value=np.matrix(np.ones([3,6]),dtype=theano.config.floatX)
 
-    w1=theano.shared(np.array(np.random.rand(6,50), dtype=theano.config.floatX))
-    w2=theano.shared(np.array(np.random.rand(50,10), dtype=theano.config.floatX))
-    w3=theano.shared(np.array(np.random.rand(10,1), dtype=theano.config.floatX))
+    w1=theano.shared(np.array(np.random.rand(6,10), dtype=theano.config.floatX))
+    w2=theano.shared(np.array(np.random.rand(10,30), dtype=theano.config.floatX))
+    w3=theano.shared(np.array(np.random.rand(30,1), dtype=theano.config.floatX))
 
 
     b1 = theano.shared(1.)
     b2 = theano.shared(1.)
     b3 = theano.shared(1.)
-    learning_rate = 0.001
     
+    learning_rate = T.scalar('learning_rate')
 
     #构造四层全连接网络
     print 'Init network'
-    tmp=T.dot(x,w1)
     #print tmp.tag.test_value
-    a1=T.nnet.relu((T.dot(x,w1)+b1))
+    a1=T.nnet.relu((T.dot(x,w1)+b1),0.1)
     #print a1.tag.test_value
 
-    a2=T.nnet.relu((T.dot(a1,w2)+b2))
+    a2=T.nnet.relu((T.dot(a1,w2)+b2),0.1)
     #print a2.tag.test_value
     
     #tmp=copy(a2)
     #tmp.extend(a1)
     #x2 = T.stack(tmp,axis=1)
-    a3 = T.nnet.relu((T.dot(a2,w3)+b3))
+    a3 = T.nnet.relu((T.dot(a2,w3)+b3),0.1)
     #print a3.tag.test_value
 
     '''
@@ -63,7 +68,9 @@ def layout_1(inputs,outputs):
     tmp=tmp**2
     print 'tmp test value 2:'
     print tmp.test_value
-    '''
+    '''    
+    a_hat = T.matrix('a_hat') #Actual output
+
     cost = T.sum((a_hat - a3)**2)
     #print 'cost test value:'
     #print cost.tag.test_value
@@ -81,7 +88,7 @@ def layout_1(inputs,outputs):
     u_list.append([b3,b3-learning_rate*db3])
     #训练函数
     train = function(
-        inputs = [x,a_hat],
+        inputs = [x,a_hat,learning_rate],
         outputs = [a1,a2,a3,w1,w2,w3,dw1,dw2,dw3,a3,cost],
         updates = u_list
         #profile=True
@@ -93,33 +100,40 @@ def layout_1(inputs,outputs):
     w_=[]
     dw_=[]
     b_size=30
-    for iteration in xrange(1000):
+    lr= 0.00001
+    itr=10000
+    end_train=False #结束标志
+
+    for iteration in xrange(itr):
         batch=b_size
         cost_iter=0
         pred=[]
+
         while batch<len(inputs):
-            a1,a2,a3,w_1,w_2,w_3,dw_1,dw_2,dw_3,p_, c_ = train(inputs[batch-b_size:batch], outputs[batch-b_size:batch])
+            a1,a2,a3,w_1,w_2,w_3,dw_1,dw_2,dw_3,p_, c_ = train(inputs[batch-b_size:batch], outputs[batch-b_size:batch],lr)
             cost_iter=cost_iter+c_
             batch=batch+b_size
             pred.extend(p_)
-        a1,a2,a3,w_1,w_2,w_3,dw_1,dw_2,dw_3,p_, c_ = train(inputs[batch-b_size:len(inputs)], outputs[batch-b_size:len(inputs)]) #last batch
+        a1,a2,a3,w1,w2,w3,dw1,dw2,dw3,p_, c_ = train(inputs[batch-b_size:len(inputs)], outputs[batch-b_size:len(inputs)],lr) #last batch
         cost_iter=cost_iter+c_
         pred.extend(p_)
+        #w_.append([w1,w2,w3])    
+        #dw_.append([dw1,dw2,dw3])
+        cost.append(cost_iter)  #######Stop here#####
 
-        w_.append([w_1,w_2,w_3])
-        dw_.append([dw_1,dw_2,dw_3])
-        cost.append(cost_iter)
-        print '###Iter: '+str(iteration)+'  cost: '+str(cost_iter)+' ###'
+        print '###Iter: '+str(iteration)+'  cost: '+str(cost_iter)+' ###'   
 
-        shuffle_exp(inputs,outputs,iteration)
-    
+        shuffle_exp(inputs,outputs,iteration)   #打乱训练集，结束一轮迭代
+        if end_train:   #结束训练
+            break
+    '''
     f1=open(dir+'\\w.txt','wb')
     f2=open(dir+'\\wb.txt','wb')
     print >>f1,w_[200:500]
     print >>f2,dw_[200:500]
     f1.close()
     f2.close()
-    
+    '''
     '''
     # 打印输出    
     print 'The outputs of the NN are:'
